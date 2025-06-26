09749968de1061187152c15a8e18f02a520aa9da
Carrington Hay Kellar <carrie@baxus.co>
2025-06-26 16:21:44 -0400

    feat: add preview configuration for allowed hosts

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,31 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 import viteCompression from "vite-plugin-compression";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [
-        react(),
-        viteCompression({
-            algorithm: "brotliCompress",
-            ext: ".br",
-            threshold: 1024,
-        }),
-    ],
-    clearScreen: false,
-    build: {
-        outDir: "dist",
-        minify: true,
-        cssMinify: true,
-        sourcemap: false,
-        cssCodeSplit: true,
+  plugins: [
+    react(),
+    viteCompression({
+      algorithm: "brotliCompress",
+      ext: ".br",
+      threshold: 1024,
+    }),
+  ],
+  clearScreen: false,
+  build: {
+    outDir: "dist",
+    minify: true,
+    cssMinify: true,
+    sourcemap: false,
+    cssCodeSplit: true,
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
     },
-    resolve: {
-        alias: {
-            "@": "/src",
-        },
-    },
+  },
+  preview: {
+    allowedHosts: ["bob-frontend.baxusprod.xyz"],
+  },
 });
